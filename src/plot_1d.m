425fc3cc240f3a381d3ca7b5eb8be21142126738
breakmit-0 <bonbaoke@@gmail.com>
2024-03-13 10:26:19 +0100

    plot

--- src/plot_1d.m
+++ src/plot_1d.m
@@ -1,16 +1,14 @@
 
-function plot_2d(oa, ob)
+function plot_1d(oa, ob)
     
     N = size(ob, 1);
     %[x, y] = meshgrid(-10:0.1:10, 10:0.1:10);
     x = linspace(-30, 30, 2);
-    y = linspace(-30, 30, 2);
-    [xx,yy] = meshgrid(x,y);
     for i = 1:N
        b = ob(i);
 
-       z = xx*oa(i,1) + yy*oa(i,2) + b;
-       surf(x, y, z);
+       y = x*oa(i,1) + b;
+       plot(x, y);
        hold on;
     end
 end
