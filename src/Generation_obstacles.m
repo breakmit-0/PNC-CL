7bb2f62e983f1d2abaeec6e14bd51df9ccbe1d83
breakmit-0 <bonbaoke@@gmail.com>
2024-03-13 09:21:44 +0100

    fix gen obstacles

--- src/Generation_obstacles.m
+++ src/Generation_obstacles.m
@@ -8,50 +8,53 @@ arguments
     space_length = 30
 end
 
-%This function takes in arguments the dimension of the work space, the space
-%length in every direction, the number of obstacles to place in this space,
-%their average size and deviation size (where the size is the distance
-%between the center of an obstacle and its points), and the standard 
-%deviation of the points of a sized obstacle, and randomly generates the 
-%obstacles in the form of convex polyhedra
+    %This function takes in arguments the dimension of the work space, the space
+    %length in every direction, the number of obstacles to place in this space,
+    %their average size and deviation size (where the size is the distance
+    %between the center of an obstacle and its points), and the standard 
+    %deviation of the points of a sized obstacle, and randomly generates the 
+    %obstacles in the form of convex polyhedra
 
-R_size = chol(standard_deviation_size^2);
-R_point = chol(points_standard_deviation^2);
+    R_size = chol(standard_deviation_size^2);
+    R_point = chol(points_standard_deviation^2);
 
-%Initialization of the list of obstacles
-P = repmat(Polyhedron(), 1, Number_of_obstacles);
+    %Initialization of the list of obstacles
+    P = repmat(Polyhedron(), Number_of_obstacles, 1);
 
-%Initialization of the matrix which associates a center to a polyhedron
-centers = zeros(dimension, Number_of_obstacles);
+    %Initialization of the matrix which associates a center to a polyhedron
+    centers = zeros(dimension, Number_of_obstacles);
 
-for i=1:Number_of_obstacles
-    %Centers are uniformly distribued in the space considering the space length 
-    centers(:,i) = rand(dimension,1)*space_length;
-    
-    %The number of points of a polyhedron is randomly chosen and takes value
-    %from dimension+1 to dimension + 11 with more polyhedra with few points
-    geometric_factor = ((rand)^3)*10;
-    Number_of_points = dimension + 1 + int32(geometric_factor);
-    
-    %each polyhedron has a size, which is randomly chosen with a gaussian
-    %distribution of parameters : the average size value and the standard
-    %deviation size
-    points = zeros(dimension, Number_of_points);
-    size = average_size + randn*R_size;
+    for i=1:Number_of_obstacles
+        %Centers are uniformly distribued in the space considering the space length 
+        centers(:,i) = rand(dimension,1)*space_length;
 
-    %then, for each point of the polyhedron, a normalized vector is
-    %uniformly chosen on the unit circle. The point is placed at the
-    %distance "size * (1 + random distance chosen with a gaussian distribution
-    %of parameters 0 and the standard deviation of the points)"
-    vector = zeros(dimension,Number_of_points);
-    for j=1:Number_of_points
-        vector(:,j) = (rand(1,dimension)-0.5)*2;
-        point_deviation = randn*R_point; 
-        points(:,j) = vector(:,j)/norm(vector(:,j))*size*(1+point_deviation);
+        %The number of points of a polyhedron is randomly chosen and takes value
+        %from dimension+1 to dimension + 11 with more polyhedra with few points
+        geometric_factor = ((rand)^3)*10;
+        Number_of_points = dimension + 1 + int32(geometric_factor);
 
-    %The new polyhedron is saved in the list
-    P(i) = Polyhedron(points') + centers(:,i);
+        %each polyhedron has a size, which is randomly chosen with a gaussian
+        %distribution of parameters : the average size value and the standard
+        %deviation size
+        points = zeros(dimension, Number_of_points);
+        size = average_size + randn*R_size;
+
+        %then, for each point of the polyhedron, a normalized vector is
+        %uniformly chosen on the unit circle. The point is placed at the
+        %distance "size * (1 + random distance chosen with a gaussian distribution
+        %of parameters 0 and the standard deviation of the points)"
+        vector = zeros(dimension,Number_of_points);
+        for j=1:Number_of_points
+            vector(:,j) = (rand(1,dimension)-0.5)*2;
+            point_deviation = randn*R_point; 
+            points(:,j) = vector(:,j)/norm(vector(:,j))*size*(1+point_deviation);
+
+            %The new polyhedron is saved in the list
+            P(i) = Polyhedron(points') + centers(:,i);
+        end
     end
+    out = P;
 end
-plot(P)
-out = P;
+
+
+
--- src/projectFacets.m
+++ src/projectFacets.m
@@ -1,7 +1,7 @@
 function [faces] = projectFacets(A, b)
-% construct associated polyhedron
-P = Polyhedron('A', A, 'b', b);
-P.minHRep();
-% take all facets of P and project them into R^(d - 1)
-faces = arrayfun(@(x) projection(x, 1:(P.Dim - 1)), P.getFacet());
+    % construct associated polyhedron
+    P = Polyhedron('A', A, 'b', b);
+    P.minHRep();
+    % take all facets of P and project them into R^(d - 1)
+    faces = arrayfun(@(x) projection(x, 1:(P.Dim - 1)), P.getFacet());
 end
