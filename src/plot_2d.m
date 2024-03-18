868688f374cc36737ab0b322689c152d5fc44ec6
breakmit-0 <bonbaoke@@gmail.com>
2024-03-18 17:48:53 +0100

    full test function in 2d

--- src/plot_2d.m
+++ src/plot_2d.m
@@ -1,22 +1,30 @@
 
-function plot_2d(a, oa, ob)
+function plot_2d(a, oa, ob, space)
     
     N = size(ob, 1);
     %[x, y] = meshgrid(-10:0.1:10, 10:0.1:10);
-    x = linspace(0, 30, 2);
-    y = linspace(0, 30, 2);
+    x = linspace(0, space, 2);
+    y = linspace(0, space, 2);
     [xx,yy] = meshgrid(x,y);
 
     plot3(0, 0, 0);
     hold on;
 
 
+    cmap = hsv(N);
+
     for i = 1:N
-       b = ob(i);
+        b = ob(i);
 
-       z = (xx*oa(i,1) + yy*oa(i,2) + b) - 100;
-       surf(x, y, z, [0 0 0]);
+        c = cmap(i, :);
+        col(1, 1, :) = c;
+        col(1, 2, :) = c;
+        col(2, 1, :) = c;
+        col(2, 2, :) = c;
 
+        z = (xx*oa(i,1) + yy*oa(i,2) + b) - 100;
+        assert_shape(z, [2 2]);
+        surf(x, y, z, col);
     end
 
     plot(a);
---
+++ src/test_2d.m
@@ -0,0 +1,9 @@
+function test_2d(n_obs, spacing)
+arguments
+    n_obs = 10;
+    spacing = 30;
+end
+    p = Generation_obstacles(2, n_obs, 1, 0.2, 0.1, spacing);
+    [oa, ob] = find_lift(p);
+    plot_2d(p, oa, ob, spacing);
+end
