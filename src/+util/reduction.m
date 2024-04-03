cfd7581a2785809084292d803441e33d753d8a3d
MickaelMartinelli <161127291+MickaelMartinelli@users.noreply.github.com>
2024-04-03 15:42:28 +0200

    Documentation reduction.m

--- src/+util/reduction.m
+++ src/+util/reduction.m
@@ -1,4 +1,21 @@
 function out = reduction(polyhedron,homothetie_factor)
+% util.reduction The function to reduce a polyhedron by the given homothetie factor around its barycenter[<a href="matlab:web('https://breakmit-0.github.io/util-ppl/')">online docs</a>]
+    % 
+    %
+    % Usage:
+    %   P = util.reduction(polyhedron, homothetie_facor)
+    %
+    % Parameters:
+    %   a Polyhedron object, polyhedron 
+    %   a scalar, homothetie_factor 
+    %
+    % Return Values:
+    %   P is a Polyhedron object of the same dimension as polyhedron
+    %   
+    %   P is the reduction of polyhedron around its barycenter by
+    %   the given homothetie factor
+    %
+    % See also util
 
 points = polyhedron.V;
 center = util.barycenter(polyhedron);
