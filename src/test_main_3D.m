52ef62bc1fea9ff9ea0184594ccb36a7156870fa
MickaelMartinelli <161127291+MickaelMartinelli@users.noreply.github.com>
2024-04-04 15:41:37 +0200

    test_main_3D

---
+++ src/test_main_3D.m
@@ -0,0 +1,17 @@
+dimension = 3;
+space_length = 30;
+src = [0 0 0];
+dest = [space_length space_length space_length];
+finder = graph.EdgePathFinder();
+
+%obstacles = testing.Counter_examples();
+obstacles = testing.generation_obstacles(dimension,10,3,0,0,space_length,100);
+
+[G, path, vertexSet] = main(obstacles, space_length, src, dest, finder);
+p = plot(G, ...
+    'XData', vertexSet.extract_coords(1), ...
+    'YData', vertexSet.extract_coords(2), ...
+    'ZData', vertexSet.extract_coords(3));
+highlight(p, path, 'EdgeColor', 'red', 'LineWidth', 3)
+hold on 
+plot(obstacles)
