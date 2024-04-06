b38d7c89f20798422c04069139be889f0a2b6f40
PoulpoGaz <valent.carrez@gmail.com>
2024-04-06 13:46:18 +0200

    add test class for read_obj. remove useHRep parameter from read_obj

--- src/+util/read_obj.m
+++ src/+util/read_obj.m
@@ -1,89 +1,39 @@
-function [polyhedra] = read_obj(objFile, useHRep)
+function [polyhedra] = read_obj(objFile)
     % util.read_obj Read a .obj file and returns a column vector of polyhedra.
     % 
     % Parameters:
     %     objFile: the file to read
-    %     useHRep: true if you want to have polyhedra directly in HRep
-    %     otherwise they will be in VRep.
     %
     % Return value:
     %     polyhedra: the loaded polyhedra.
 
-    if nargin < 2
-        useHRep = false;
-    end
-    
     fid = fopen(objFile, 'r');
     assert(fid ~= -1, "Cannot open file");
 
     polyhedra = [];
 
-    if useHRep
-        % parse obj with HRep in mind
-        % need to create A and b and keep every vertices in a set.
-        A = [];
-        b = [];
-        vertices = [];
-        offset = 0;
+    % parse obj with VRep in mind.
+    % we don't need to have a vertex set nor checking faces.
+    vertices = [];
+    while true    
+        tline = fgetl(fid);
+        if ~ischar(tline), break, end % check EOF
+        
+        ln = sscanf(tline,'%s',1); % line type 
 
-        while true    
-            tline = fgetl(fid);
-            if ~ischar(tline), break, end % check EOF
-            
-            ln = sscanf(tline,'%s',1); % line type 
-    
-            switch ln
-                case 'v' % mesh vertexs
-                    vertices = [vertices; sscanf(tline(2:end),'%f')'];
-                case 'f'
-                    face = sscanf(tline(2:end),'%d')';
-                    V1 = vertices(face(1) - offset, :);
-                    V2 = vertices(face(2) - offset, :);
-                    V3 = vertices(face(3) - offset, :);
-
-                    normal = cross(V2 - V1, V3 - V1);
-                    A = [A; normal];
-                    b = [b; dot(normal, V1)];
-                case 'o' % new object
-                    if height(A) > 0
-                        polyhedra = [polyhedra; Polyhedron('A', A, 'b', b)];
-                        offset = offset + height(vertices);
-                        A = [];
-                        b = [];     
-                        vertices = [];
-                    end
-            end
+        switch ln
+            case 'v' % mesh vertexs
+                vertices = [vertices; sscanf(tline(2:end),'%f')' ];
+            case 'o' % new object
+                if height(vertices) > 0
+                    polyhedra = [polyhedra; Polyhedron('V', vertices)];
+                    vertices = [];
+                end
         end
+    end
 
-        if height(A) > 0
-            polyhedra = [polyhedra; Polyhedron('A', A, 'b', b)];  
-        end
-
-
-    else
-        % parse obj with VRep in mind.
-        % we don't need to have a vertex set nor checking faces.
-        vertices = [];
-        while true    
-            tline = fgetl(fid);
-            if ~ischar(tline), break, end % check EOF
-            
-            ln = sscanf(tline,'%s',1); % line type 
-    
-            switch ln
-                case 'v' % mesh vertexs
-                    vertices = [vertices; sscanf(tline(2:end),'%f')' ];
-                case 'o' % new object
-                    if height(vertices) > 0
-                        polyhedra = [polyhedra; Polyhedron('V', vertices)];
-                        vertices = [];
-                    end
-            end
-        end
-
-        if height(vertices) > 0
-            polyhedra = [polyhedra; Polyhedron('V', vertices)];
-        end
+    if height(vertices) > 0
+        polyhedra = [polyhedra; Polyhedron('V', vertices)];
     end
    
     fclose(fid);
