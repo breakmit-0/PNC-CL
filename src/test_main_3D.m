de496f0cc4ee3b93068a1c1868cd605639166efd
MickaelMartinelli <161127291+MickaelMartinelli@users.noreply.github.com>
2024-04-27 00:57:40 +0200

    Corridors test_main_3D.m

--- src/test_main_3D.m
+++ src/test_main_3D.m
@@ -10,12 +10,16 @@ obstacles = testing.generation_obstacles(dimension,10,3,0,0,space_length,100);
 %obstacles = util.read_obj("src/+testing/obj/3_cubes.obj").';
 bbx = util.bounding_box(obstacles, 1.25, false);
 
-[P, G, vertexSet, path, dist] = main(obstacles, bbx, src, dest, gBuilder);
+[P, G, vertexSet, path, dist, corridors, d] = main(obstacles, bbx, src, dest, gBuilder);
 
 disp("Path length: " + dist)
+disp("Path width: " + d)
 P.plot('alpha', .1, 'color', 'lightblue');
 hold on
 obstacles.plot('color', 'r');
+
+plot(corridors,'alpha',0.5,'color',[0.5 0.5 0],'edgealpha',0)
+
 p = plot(G, ...
     'XData', vertexSet.extractCoords(1), ...
     'YData', vertexSet.extractCoords(2), ...
