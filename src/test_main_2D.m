764fd26f255fdd477074d7e97c0b4c3cd3a1e514
breakmit-0 <bonbaoke@@gmail.com>
2024-04-26 11:12:08 +0200

    fast lift

--- src/test_main_2D.m
+++ src/test_main_2D.m
@@ -1,11 +1,11 @@
 dimension = 2;
-space_length = 30;
+space_length = 40;
 src = [-space_length/2 -space_length/2];
 dest = [space_length/2 space_length/2];
 gBuilder = graph.BarycenterGraphBuilder();
 
 %obstacles = testing.Counter_examples();
-obstacles = testing.generation_obstacles(dimension,10,1,0,0,space_length,100);
+obstacles = testing.generation_obstacles(dimension,80,1,0,0,space_length,100);
 bbx = util.bounding_box(obstacles, 1.25, true);
 
 [P, G, vertexSet, path, dist] = main(obstacles, bbx, src, dest, gBuilder);
