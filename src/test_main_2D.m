344f35fd8b14d9580f3d260ffe50eeaffa1aabef
MickaelMartinelli <161127291+MickaelMartinelli@users.noreply.github.com>
2024-04-27 00:56:18 +0200

    Corridors test_main_2D.m

--- src/test_main_2D.m
+++ src/test_main_2D.m
@@ -8,12 +8,15 @@ gBuilder = graph.BarycenterGraphBuilder();
 obstacles = testing.generation_obstacles(dimension,80,1,0,0,space_length,100);
 bbx = util.bounding_box(obstacles, 1.25, true);
 
-[P, G, vertexSet, path, dist] = main(obstacles, bbx, src, dest, gBuilder);
+[P, G, vertexSet, path, dist, corridors, d] = main(obstacles, bbx, src, dest, gBuilder);
 
 disp("Path length: " + dist)
+disp("Path width: " + d)
 P.plot('color', 'lightblue')
 hold on
 obstacles.plot('color', 'r')
+
+plot(corridors,'alpha',0.5,'color',[0.5 0.5 0],'edgealpha',0)
 
 p = plot(G, ...
     'XData', vertexSet.extractCoords(1), ...
