b7aff4bc5f07de9ee87785a1024b36c01c86e943
PoulpoGaz <valent.carrez@gmail.com>
2024-04-27 18:09:22 +0200

    merge with origin/master

--- src/test_main_2D.m
+++ src/test_main_2D.m
@@ -5,7 +5,7 @@ dest = [space_length/2 space_length/2];
 gBuilder = graph.BarycenterGraphBuilder();
 
 %obstacles = testing.Counter_examples();
-obstacles = testing.generation_obstacles(dimension,80,1,0,0,space_length,100);
+obstacles = testing.generation_obstacles(dimension,10,4,0,0,space_length,100);
 bbx = util.bounding_box(obstacles, 1.25, true);
 
 [P, G, vertexSet, path, dist] = main(obstacles, bbx, src, dest, gBuilder);
