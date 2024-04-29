095d58dc58d29ce77e2586db42aec42fdd610e02
PoulpoGaz <valent.carrez@gmail.com>
2024-04-29 16:32:04 +0200

    fix EdgeGraphBuilder crashing

--- src/+graph/EdgeGraphBuilder.m
+++ src/+graph/EdgeGraphBuilder.m
@@ -2,7 +2,7 @@ classdef EdgeGraphBuilder < graph.GraphBuilder
 
     methods    
         function G = buildGraph(obj, partition)
-            edges = alt_graph.edges(partition);
+            edges = alt_graph.find_edges(partition);
             G = alt_graph.gen_graph(edges);
         end
     end
--- src/test_main_3D.m
+++ src/test_main_3D.m
@@ -4,7 +4,7 @@ dimension = 3;
 space_length = 30;
 src = [-space_length/2 -space_length/2 -space_length/2];
 dest = [space_length/2 space_length/2 space_length/2];
-gBuilder = graph.EdgeGraphBuilder();
+gBuilder = graph.BarycenterGraphBuilder();
 
 obstacles = testing.generation_obstacles(dimension,10,3,0,0,space_length,100);
 %obstacles = util.read_obj("src/+testing/obj/3_cubes.obj").';
