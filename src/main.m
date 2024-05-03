65dc9121c76045d49ef0ff02e64287ad3f40f478
MickaelMartinelli <161127291+MickaelMartinelli@users.noreply.github.com>
2024-05-03 10:41:43 +0200

    path take corridors width into account

--- src/main.m
+++ src/main.m
@@ -1,8 +1,8 @@
-function [P, G, path, corridors, d] = main(obstacles, bbx, src, dest, graphBuilder)
+function [P, G, path, Corridors, width] = main(obstacles, bbx, src, dest, graphBuilder)
 % main [<a href="matlab:web('https://breakmit-0.github.io/testing-ppl/')">online docs</a>]
     %
     % Usage:
-    %   P = main(obstales, space_length, src, dest, barycenterpath)
+    %   P = main(obstales, bbx, src, dest, graphBuilder)
     %
     % Parameters:
     %
@@ -27,11 +27,19 @@ G = graphBuilder.buildGraph(P);
 disp("Graph build in " + toc + "s")
 
 tic
+G = corridors.corridor_width(G, obstacles);
+disp("Corridors width computed in " + toc + "s")
+
+tic
+G = corridors.edge_weight(G);
+disp("Edges weight adjusted in " + toc + "s")
+
+tic
 path = alt_graph.path(G, src, dest, obstacles);
 disp("Path found in " + toc + "s")
 
 tic
-[corridors, d] = corridor(G.Nodes.position, G.Edges, obstacles, 100);
-disp("Corridors computed in " + toc + "s")
+[Corridors, width] = corridors.corridor(G, path, 100);
+disp("Corridors described in " + toc + "s")
 
 end
