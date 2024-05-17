f0d49baf0277afe840a19a53f092b349e2546efc
MickaelMartinelli <161127291+MickaelMartinelli@users.noreply.github.com>
2024-05-17 09:52:08 +0200

    Update test_main_2D.m with corridors

--- src/test_main_2D.m
+++ src/test_main_2D.m
@@ -1,22 +1,21 @@
 dimension = 2;
-space_length = 50;
+space_length = 40;
 src = [-space_length/2 -space_length/2];
 dest = [space_length/2 space_length/2];
-gBuilder = graph.EdgeGraphBuilder();
-gBuilder.parallel = true;
+gBuilder = graph.BarycenterGraphBuilder();
 
 %obstacles = testing.Counter_examples();
-obstacles = testing.generation_obstacles(dimension,100,1,0,0,space_length,100);
+obstacles = testing.generation_obstacles(dimension,10,5,0,0,space_length,100);
 bbx = util.bounding_box(obstacles, 1.25, true);
 
-[P, G, path, corridors, d] = main(obstacles, bbx, src, dest, gBuilder);
+[P, G, path, corridors, width, dist] = main(obstacles, bbx, src, dest, gBuilder);
 
-% disp("Path length: " + dist)
-disp("Path width: " + d)
+disp("Path length: " + dist)
+disp("Path width: " + width)
 P.plot('color', 'lightblue')
 hold on
 obstacles.plot('color', 'r')
 
-plot(corridors,'alpha',0.5,'color',[0.5 0.5 0],'edgealpha',0)
+plot(corridors,'alpha',0.5,'color',[1 1 0],'edgealpha',0)
 alt_graph.plot_graph(G);
 alt_graph.plot_path(G, src, dest, path);
