62f2a5f85fd1de895fa27cf40436b4fd93e468f3
MickaelMartinelli <161127291+MickaelMartinelli@users.noreply.github.com>
2024-06-10 21:39:03 +0200

    Patch alt_graph.plot_graph -> testing.plot_graph

--- +testing/test_main_3D.m
+++ +testing/test_main_3D.m
@@ -8,7 +8,7 @@ obstacles = testing.generation_obstacles(dimension,30,10,0,0,space_length,100);
 %obstacles = util.read_obj("src/+testing/obj/3_cubes.obj").';
 bbx = util.bounding_polyhedron(obstacles, true, 1.25);
 
-[P, G, path, corridors, width, dist] = testing.main(obstacles, bbx, src, dest, gBuilder);
+[P, G, path, Corridors, width, dist] = testing.main(obstacles, bbx, src, dest, gBuilder);
 
 disp("Path length: " + dist)
 disp("Path width: " + width)
@@ -16,10 +16,10 @@ P.plot('alpha', .1, 'color', 'lightblue');
 hold on
 obstacles.plot('color', 'r');
 
-plot(corridors,'alpha',0.5,'color',[1 1 0],'edgealpha',0)
+plot(Corridors,'alpha',0.5,'color',[1 1 0],'edgealpha',0)
 
-alt_graph.plot_graph(G);
-alt_graph.plot_path(G, src, dest, path);
+testing.plot_graph(G);
+testing.plot_path(G, src, dest, path);
 
 
 mpc_input = mpcsettings();
@@ -62,7 +62,7 @@ ctrl = mpc(mpc_input);
 
 pathS = struct('V',table2array(G.Nodes(path,:)));
 x0 = pinv(ctrl.C) * pathS.V(1,:)';
-[ts, trajectory, controller] = ctrl.relayMPC(pathS, corridors, x0);
+[ts, trajectory, controller] = ctrl.relayMPC(pathS, Corridors, x0);
 
 plot3(trajectory(:,1),trajectory(:,2),trajectory(:,3),'Marker','*','LineStyle','-','color',[0 0 0],'LineWidth',1.5);
 
