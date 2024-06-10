7be9eee5764d4e5b5315eac59978c72331e97dab
MickaelMartinelli <161127291+MickaelMartinelli@users.noreply.github.com>
2024-06-10 21:48:18 +0200

    Update test_robot_2D.m

--- +testing/test_robot_2D.m
+++ +testing/test_robot_2D.m
@@ -7,17 +7,19 @@ gBuilder = graph.EdgeGraphBuilder();
 %obstacles = testing.Counter_examples();
 obstacles = testing.generation_obstacles(dimension,10,3,0,0,space_length,100);
 robot = testing.generation_obstacles(dimension,1,4,0,0,space_length,100);
-bbx = util.bounding_polyhedron(obstacles, 1.25, true);
+bbx = util.bounding_polyhedron(obstacles, true, 1.25);
 
-[P, G, path, corridors, width, dist] = testing.main_robot(obstacles, bbx, src, dest, gBuilder,robot);
+[P, G, path, Corridors, width, dist] = testing.main_robot(obstacles, bbx, src, dest, gBuilder,robot);
+robot_radius = util.radius(robot);
 
 disp("Path length: " + dist)
 disp("Path width: " + width)
+disp("Robot radius; " + robot_radius)
 P.plot('color', 'lightblue')
 hold on
 obstacles.plot('color', 'r')
 
-plot(corridors,'alpha',0.5,'color',[1 1 0],'edgealpha',0)
+plot(Corridors,'alpha',0.5,'color',[1 1 0],'edgealpha',0)
 testing.plot_graph(G);
 testing.plot_path(G, src, dest, path);
-robot.plot('color', 'g')
+robot.plot('color', 'g', 'alpha', 0.1,'edgealpha',0.2)
